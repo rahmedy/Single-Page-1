ca1dbeaf9238113bd175286a04d6e300632e8506
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2021-01-08 17:53:17 -0500

    hosting

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const helmet = require('helmet');
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+require("./routes/html-routes.js")(app);
+
 const port = process.env.PORT || 3020;
 app.listen(port, () => {
 	console.log(`listening at http://localhost:${port}`);
